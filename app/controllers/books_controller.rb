631449a40bdfc6ba75f3ecd564af46ff9b11b521
kento-96 <kento.soccer.1107@gmail.com>
2021-04-22 02:06:42 +0000

    [create]edit_view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,37 @@
 class BooksController < ApplicationController
+
+ def index
+  @book=Book.new
+  @books=Book.all
+
+ end
+
+ def create
+  book=Book.new(book_params)
+  book.save
+  redirect_to books_path
+ end
+
+ def show
+   @book=Book.find(params[:id])
+ end
+
+ def edit
+  @book=Book.find(params[:id])
+ end
+ 
+ def update
+ book=Book.find(params[:id])
+ book.update(book_params)
+ redirect_to book_path(book.id)
+ end
+
+ def destroy
+ end
+
+ private
+ def book_params
+  params.require(:book).permit(:title,:body)
+ end
+
 end
